83d8aa921ab6f5e958c16d37240653bdd2c23460
Vinicius Mota <vinicius.motamorici@gmail.com>
2024-02-25 13:37:48 -0300

    Add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import {App} from './app'
 import './index.css'
 import { Toaster } from 'sonner'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/>
     <App />
     <Toaster richColors/>
   </React.StrictMode>,
